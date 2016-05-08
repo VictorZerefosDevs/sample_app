fb8fb5b25517555a6aeaf1ba444df8ff1fcc9a1c
Victor Zerefos <zvelopswebsite@gmail.com>
2016-05-08 02:09:37 -0300

    set up test helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
+  get 'home' => 'static_pages#home'
 
-  get 'static_pages/help'
+  get 'help' => 'static_pages#help'
 
   root 'application#hello'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
